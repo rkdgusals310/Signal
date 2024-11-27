b9392d37ae5037bd7c30b1a6e5eff8dd5d49bff2
ChangOKIM <kk104202@naver.com>
2024-11-27 19:03:55 +0900

    fix:채팅룸바로아래

--- src/main/frontend/src/pages/ChatRoomPage.js
+++ src/main/frontend/src/pages/ChatRoomPage.js
@@ -35,7 +35,7 @@ const ChatRoomPage = () => {
 
   const scrollToBottom = () => {
     if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+      messagesEndRef.current.scrollIntoView({ behavior: 'auto' });
     }
   };
 
